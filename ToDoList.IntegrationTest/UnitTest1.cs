77ee31ede5bea4a287d882b862e4c01381bbe839
Hamza Juwaihan <hamzajuwaihan@gmail.com>
2024-07-04 22:52:34 +0300

    Add project files.

---
+++ ToDoList.IntegrationTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ToDoList.IntegrationTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ ToDoListAPI.ServiceTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ToDoListAPI.ServiceTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
