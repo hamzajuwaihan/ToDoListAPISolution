b1b1e6b4263c2cd8a45d67ca2e13aeca5bc95752
Hamza Juwaihan <hamzajuwaihan@gmail.com>
2024-07-05 22:56:55 +0300

    Perpare configure service extension and inject app services

--- ToDoListAPI.Web/Program.cs
+++ ToDoListAPI.Web/Program.cs
@@ -1,6 +1,13 @@
+using ToDoListAPI.Web.StartupExtensions;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Inject and configure necessary services for the application.
+builder.Services.ConfigureServices(builder.Configuration);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
 
 app.Run();
---
+++ ToDoListAPI.Web/StartupExtensions/ConfigureServicesExtension.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ToDoListAPI.Core.Domain.IdentityEntities;
+using ToDoListAPI.Core.Services;
+using ToDoListAPI.Core.ServicesContracts;
+using ToDoListAPI.Infastructure.Db;
+
+namespace ToDoListAPI.Web.StartupExtensions
+{
+    public static class ConfigureServicesExtension
+    {
+        public static IServiceCollection ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddControllers();
+
+            services.AddScoped<IAddTodoTaskService, AddTodoTaskService>();
+            services.AddScoped<IDeleteTodoTaskService, DeleteTodoTaskService>();
+            services.AddScoped<IGetTodoTaskService, GetTodoTaskService>();
+            services.AddScoped<IUpdateTodoTaskService, UpdateTodoTaskService>();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddIdentity<ApplicationUser, IdentityRole<Guid>>()
+             .AddEntityFrameworkStores<ApplicationDbContext>()
+             .AddDefaultTokenProviders();
+
+            return services;
+        }
+    }
+}
