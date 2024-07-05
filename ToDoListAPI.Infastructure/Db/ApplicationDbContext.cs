1b72270689c5723dbdfbc7e9004400b18d38bf50
Hamza Juwaihan <hamzajuwaihan@gmail.com>
2024-07-05 22:56:25 +0300

    Add Task repository implements task repository contract (functionality not implemented yet), add ApplicationDbContext class

---
+++ ToDoListAPI.Infastructure/Db/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ToDoListAPI.Core.Domain.IdentityEntities;
+using Microsoft.AspNetCore.Identity;
+using ToDoListAPI.Core.Domain.Entities;
+
+namespace ToDoListAPI.Infastructure.Db
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TodoTask> TodoTasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+    }
+}
---
+++ ToDoListAPI.Infastructure/Repositories/TasksRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoListAPI.Core.Domain.Entities;
+using ToDoListAPI.Core.Domain.RepositoryContracts;
+
+namespace ToDoListAPI.Infastructure.Repositories
+{
+    public class TasksRepository : ITasksRepository
+    {
+        public Task<TodoTask> AddTask(TodoTask task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteTask(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TodoTask> GetTaskById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TodoTask>> GetTasks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TodoTask> UpdateTask(TodoTask task)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
