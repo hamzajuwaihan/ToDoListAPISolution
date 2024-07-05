7cc56c5550ab1b6ae1abb095e5745c55a8d18533
Hamza Juwaihan <hamzajuwaihan@gmail.com>
2024-07-05 22:53:52 +0300

    Add Tasks repoistory contract for DB operations

---
+++ ToDoListAPI.Core/Domain/RepositoryContracts/ITasksRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoListAPI.Core.Domain.Entities;
+
+namespace ToDoListAPI.Core.Domain.RepositoryContracts
+{
+    public interface ITasksRepository
+    {
+
+        public Task<TodoTask> AddTask(TodoTask task);
+
+        public Task<TodoTask> GetTaskById(Guid id);
+
+        public Task<IEnumerable<TodoTask>> GetTasks();
+
+        public Task<TodoTask> UpdateTask(TodoTask task);
+
+        public Task<bool> DeleteTask(Guid id);
+
+    }
+}
